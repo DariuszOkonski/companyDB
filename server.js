b66ca12d4f8846f6aeec05cd241033517a64d2ef
Dariusz Okoński <darek200180@gmail.com>
2025-09-10 07:07:40 +0200

    add db to req

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ mongoClient.connect(
       console.log(err);
     } else {
       console.log('Successfully connected to the MongoDB...');
+      const db = client.db('companyDB');
+
       const employeesRoutes = require('./routes/employees.routes');
       const departmentsRoutes = require('./routes/departments.routes');
       const productsRoutes = require('./routes/products.routes');
@@ -18,6 +20,11 @@ mongoClient.connect(
       app.use(cors());
       app.use(express.json());
       app.use(express.urlencoded({ extended: false }));
+
+      app.use((req, res, next) => {
+        req.db = db;
+        next();
+      });
 
       app.use('/api', employeesRoutes);
       app.use('/api', departmentsRoutes);
