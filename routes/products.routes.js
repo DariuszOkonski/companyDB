e6fc6c1876e3709a9ae82e18bc52d7c76dca0931
Dariusz Okoński <darek200180@gmail.com>
2025-09-10 07:59:43 +0200

    add get for products

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -5,7 +5,16 @@ const router = express.Router();
 const db = require('./../db');
 
 router.get('/products', (req, res) => {
-  res.json(db.products);
+  req.db
+    .collection('products')
+    .find()
+    .toArray()
+    .then((data) => {
+      return res.json(data);
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: err });
+    });
 });
 
 router.get('/products/random', (req, res) => {
@@ -13,23 +22,25 @@ router.get('/products/random', (req, res) => {
 });
 
 router.get('/products/:id', (req, res) => {
-  res.json(db.products.find(item => item.id == req.params.id));
+  res.json(db.products.find((item) => item.id == req.params.id));
 });
 
 router.post('/products', (req, res) => {
   const { name, client } = req.body;
-  db.products.push({ id: 3, name, client })
+  db.products.push({ id: 3, name, client });
   res.json({ message: 'OK' });
 });
 
 router.put('/products/:id', (req, res) => {
   const { name, client } = req.body;
-  db = db.products.map(item => (item.id == req.params.id) ? { ...item, name, client } : item );
+  db = db.products.map((item) =>
+    item.id == req.params.id ? { ...item, name, client } : item
+  );
   res.json({ message: 'OK' });
 });
 
 router.delete('/products/:id', (req, res) => {
-  db = db.products.filter(item => item.id != req.params.id)
+  db = db.products.filter((item) => item.id != req.params.id);
   res.json({ message: 'OK' });
 });
 
