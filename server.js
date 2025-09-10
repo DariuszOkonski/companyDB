71405727c35be95a24ad264f8dab48993832529c
Dariusz Okoński <darek200180@gmail.com>
2025-09-10 06:48:47 +0200

    move endpoints to mongoClient connection

--- server.js
+++ server.js
@@ -11,26 +11,25 @@ mongoClient.connect(
       console.log(err);
     } else {
       console.log('Successfully connected to the MongoDB...');
+      const employeesRoutes = require('./routes/employees.routes');
+      const departmentsRoutes = require('./routes/departments.routes');
+      const productsRoutes = require('./routes/products.routes');
+
+      app.use(cors());
+      app.use(express.json());
+      app.use(express.urlencoded({ extended: false }));
+
+      app.use('/api', employeesRoutes);
+      app.use('/api', departmentsRoutes);
+      app.use('/api', productsRoutes);
+
+      app.use((req, res) => {
+        res.status(404).send({ message: 'Not found...' });
+      });
+
+      app.listen('8000', () => {
+        console.log('Server is running on port: 8000...');
+      });
     }
   }
 );
-
-const employeesRoutes = require('./routes/employees.routes');
-const departmentsRoutes = require('./routes/departments.routes');
-const productsRoutes = require('./routes/products.routes');
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use('/api', employeesRoutes);
-app.use('/api', departmentsRoutes);
-app.use('/api', productsRoutes);
-
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
-});
-
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000...');
-});
