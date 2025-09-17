8670f068233988f58c020f3542db6ed7744d5938
Dariusz Okoński <darek200180@gmail.com>
2025-09-17 08:48:21 +0200

    add base file for post tests for departments.routes

---
+++ test/api/departments/post.test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../server.js');
+const Department = require('../../../model/department.model.js');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('POST /api/departments', () => {
+  it('/ should insert new document to db and return success', async () => {});
+});
