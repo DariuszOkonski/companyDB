f82c6c5d627fb125f2244565de76c1bf6929cf68
Dariusz Okoński <darek200180@gmail.com>
2025-09-18 06:43:25 +0200

    add post.test for departments.controller

--- test/api/departments/post.test.js
+++ test/api/departments/post.test.js
@@ -9,5 +9,18 @@ const expect = chai.expect;
 const request = chai.request;
 
 describe('POST /api/departments', () => {
-  it('/ should insert new document to db and return success', async () => {});
+  after(async () => {
+    await Department.deleteMany();
+  });
+
+  it('/ should insert new document to db and return success', async () => {
+    const res = await request(server)
+      .post('/api/departments')
+      .send({ name: '#Department #1' });
+    const newDepartment = await Department.findOne({ name: '#Department #1' });
+
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.an('object');
+    expect(newDepartment).to.not.be.null;
+  });
 });
